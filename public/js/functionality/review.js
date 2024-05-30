159bd3bb66d5460f063e6547145d186a86516048
Shivam Sahni <shivamsahni507@gmail.com>
2024-05-30 13:45:23 +0530

    review functionality and frontend improvement

--- public/js/functionality/review.js
+++ public/js/functionality/review.js
@@ -17,10 +17,11 @@ det.addEventListener('click', () => {
 rev.addEventListener('click', () => {
   review.classList.add('display');
   detail.classList.add('display-none');
+  write_review.classList.remove('display');
 });
 give_review.addEventListener('click', () => {
   write_review.classList.add('display');
-  // review.classList.remove('display');
+  review.classList.remove('display');
   detail.classList.remove('display');
 });
 
@@ -37,20 +38,3 @@ function increaseQuantity() {
   var value = parseInt(input.value, 10);
   input.value = value + 1;
 }
-
-document.getElementById('give_review').addEventListener('click', function () {
-  const div1 = document.getElementById('review_list');
-  const div2 = document.getElementById('write_review');
-
-  // Get the parent nodes
-  const parent1 = review_list.parentNode;
-
-  const parent2 = write_review.parentNode;
-
-  // Get the next sibling of div1
-  const nextSibling1 = review_list.nextSibling;
-
-  // Swap the positions
-  parent2.insertBefore(review_list, write_review);
-  parent1.insertBefore(write_review, nextSibling1);
-});
